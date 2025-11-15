78ced4800a248c04de17bdf2cc553e86222f142d
maningreen <flibbers@protonmail.com>
2025-11-15 12:57:42 -0700

    silly

--- shell.nix
+++ shell.nix
@@ -1,5 +1,14 @@
-{ pkgs ? import <nixpkgs> {}, ... }: pkgs.mkShell {
+{pkgs ? import <nixpkgs> {}, ...}:
+pkgs.mkShell {
   packages = with pkgs; [
-    qmk
+    gcc-arm-embedded
+    avrdude
+    dfu-programmer
+    dfu-util
+    uv
+    pkgsCross.avr.buildPackages.gcc
   ];
+  shellHook = ''
+    export PATH="$HOME/.local/bin:$PATH"
+  '';
 }
